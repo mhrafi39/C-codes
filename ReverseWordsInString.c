42ed36edae961dd6eb7b019c7f853cd8476b4acb
Mehedi Hasan Rafi <136667687+mhrafi39@users.noreply.github.com>
2023-08-17 09:46:16 +0600

    Reverse Words in String
    This a a code how  to reverse each words in a string.
    Example: Hello World David
    Output: olleH dlroW divaD

---
+++ ReverseWordsInString.c
@@ -0,0 +1,25 @@
+//Reverse each word Character in a string
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+    char str[100]= {"Hello World David"};
+    int l=strlen(str);
+    int j,k=0;
+
+    for(int i=0; i<=l; i++)
+    {
+        if(str[i]==' '||i==l)
+        {
+            for(j=i-1; k<j; j--,k++)
+            {
+                char t=str[k];
+                str[k]=str[j];
+                str[j]=t;
+            }
+            k=i+1;
+        }
+    }
+    puts(str);
+}
